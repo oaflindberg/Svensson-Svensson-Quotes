edb94b647a24590f467f3df836b26b26c1052c51
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-09-28 21:14:23 +0200

    Added favicon and cors

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ require('dotenv').config();
 import express = require('express');
 const app = express();
 import chalk = require('chalk');
+import cors = require('cors');
 let port = process.env.PORT || 3000;
 
 const home = require('./routes/home');
 const quotes = require('./routes/quote');
 
+app.use(cors());
 app.use(home);
 app.use(quotes);
 
