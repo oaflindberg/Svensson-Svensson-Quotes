df744d6a41c3cf1aef63fcfc7211fb72521cb910
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-09-28 17:22:42 +0200

    Reset

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,10 @@ const app = express();
 import chalk = require('chalk');
 let port = process.env.PORT || 3000;
 
-app.use(express.static('views'));
-app.use('/', express.static('views'));
-
-// const home = require('./routes/home');
+const home = require('./routes/home');
 const quotes = require('./routes/quote');
 
-// app.use(home);
+app.use(home);
 app.use(quotes);
 
 app.listen(port, () => {
