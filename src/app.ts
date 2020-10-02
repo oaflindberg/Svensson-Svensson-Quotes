b747c7eb196073b4796ebdbd475a5a71000f0c3b
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-10-02 09:59:33 +0200

    Added GitHub action

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-require('dotenv').config();
-import express = require('express');
-const app: express.Application = express();
-import chalk = require('chalk');
-import cors = require('cors');
-let port = process.env.PORT || 3000;
+require('dotenv').config()
+import express = require('express')
+const app: express.Application = express()
+import chalk = require('chalk')
+import cors = require('cors')
+let port = process.env.PORT || 3000
 
-const home = require('./routes/home');
-const quotes = require('./routes/quote');
+const home = require('./routes/home')
+const quotes = require('./routes/quote')
 
-app.use(cors());
-app.use(home);
-app.use(quotes);
+app.use(cors())
+app.use(home)
+app.use(quotes)
 
 app.listen(port, () => {
-  console.log(chalk.blue(`App listening on port http://localhost:${port}`));
-});
+  console.log(chalk.blue(`App listening on port http://localhost:${port}`))
+})
